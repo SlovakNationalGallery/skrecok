fb67501617005a3df607931bb04064da455bb31e
Philo van Kemenade <philovankemenade@gmail.com>
2019-04-01 15:24:18 +0200

    init first version w hardcoded placeholder content

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Patrick+Hand');
+html {
+  font-size: 32px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  position: relative;
+}
+
+body {
+  background-color: #ECDBB3;
+  background-image: url('/bg-skrecok.jpg');
+  background-repeat: repeat;
+  font-family: 'Patrick Hand', cursive, Arial, sans-serif;
+  color: #000;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,158 @@
+<template>
+  <div>
+    <img src="/skrecok-lamp.png" class="bg-image lamp">
+    <div class="d-none d-sm-block">
+      <img src="/skrecok-withbat-left.png" class="bg-image withbat" 
+        v-bind:style="{
+          left: this.batOffset + 'px',
+          width: this.batWidth + 'px',
+        }"
+      >
+      <img src="/skrecok-withbat-right.png" class="bg-image withbat" 
+        v-bind:style="{
+          right: this.batOffset + 'px',
+          width: this.batWidth + 'px',
+        }"
+      >
+      <img src="/skrecok-ball.png" class="bg-image ball"
+        v-bind:style="{ 
+          left: this.batOffset + 0.5 * this.batWidth + this.ballOffset + 'px',
+          width: this.ballWidth + 'px',
+        }"
+      >
+    </div>
+    
+    <section class="container">
+      <div>
+        <h1 class="title">
+          Skrecok
+        </h1>
+        <h3 class="subtitle">
+          SNG video series
+        </h3>
+      </div>
+    </section>
+    
+    <section class="container">
+      <h2>Videos</h2>
+      <ul>
+        <li v-for="youtubeVideo in youtubeVideos" class="playlistItem">
+          <h3>{{youtubeVideo.title}}</h3>
+          <youtube :video-id="youtubeVideo.id" class="youtube-video" ></youtube>
+        </li>
+      </ul>
+    </section>
+
+    <section class="container">
+      <h2>About</h2>
+    </section>
+
+    <footer class="container">
+      Footer Content
+    </footer>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    // Logo
+  },
+  data () {
+    return { 
+      youtubeVideos:  [
+        {
+          id: 'C6NquYD0bg8',
+          title: 'Z akadémie do prírody'
+        },
+        {
+          id: 'IdwC8Plxw1M',
+          title: 'Detská noc literatúry v Schaubmarovom mlyne'
+        },
+        {
+          id: 'E1lgwn5o2YY',
+          title: 'Nové akvizície SNG: Leopold Horovitz'
+        },
+      ],
+      ballOffset: 0,
+      ballRange: 0,
+      ballWidth: 30,
+      batWidth: 80,
+      batOffset: 0,
+    }
+  },
+  methods: {
+    handleScroll () {
+      let scrollPosNormalised = window.scrollY / window.innerHeight;
+      this.ballOffset = this.zigzag(scrollPosNormalised) * this.ballRange;
+    },
+    zigzag (x) {
+      return Math.acos(Math.cos(x * Math.PI)) / Math.PI;
+    }
+  },
+  beforeMount () {
+    window.addEventListener('scroll', this.handleScroll);
+    this.ballRange = window.innerWidth - 2 * (this.batOffset + 0.5 * this.batWidth) - this.ballWidth;
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+}
+</script>
+
+<style lang="scss">
+  .container {
+    // margin: 4rem auto; 
+    text-align: center;
+  }
+  ul {
+    padding: 0;
+  }
+  li {
+    list-style: none;
+  }
+  .playlistItem {
+    margin-top: 2rem;
+  }
+  .bg-image {
+    position: absolute;
+    z-index: -1;
+  }
+  .bg-image.lamp {
+    top: 0;
+    left: 0;
+  }
+  .bg-image.withbat {
+    z-index: 2;
+    position: fixed;
+    bottom: 20px;
+  }
+  .bg-image.ball {
+    position: fixed;
+    z-index: 1;
+    bottom: 110px;
+  }
+
+  @media (max-width: 991.98px) {
+    .youtube-video {
+      position: relative;
+      padding-bottom: 56.25%;
+      // padding-top: 30px;
+      height: 0;
+      overflow: hidden;
+      max-width: 640px;
+      margin: auto;
+      iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+
+</style>
