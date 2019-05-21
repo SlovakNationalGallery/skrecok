4d27417f52ab3514649c8ea56ba14876b066e659
Philo van Kemenade <philovankemenade@gmail.com>
2019-05-21 11:28:13 +0200

    add 'allow presentation' permission to sandbox attribute

--- components/VideoSection.vue
+++ components/VideoSection.vue
@@ -4,7 +4,7 @@
       <iframe 
         :src="'https://www.youtube.com/embed/'+videoId+'?modestbranding=1&rel=0'" 
         frameborder="0"
-        sandbox="allow-same-origin allow-scripts"
+        sandbox="allow-same-origin allow-scripts allow-presentation"
       ></iframe>
     </div>
     
