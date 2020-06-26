e63e8597c20a4bf58a4ce4084d216c2e734b337f
memb <ustengrav@users.noreply.github.com>
2020-06-26 17:25:13 +0200

    edit h1 in duchamp.vue

--- pages/duchamp.vue
+++ pages/duchamp.vue
@@ -15,7 +15,7 @@
         <h1
           class="my-5 bg-dark text-light tilted d-inline-block px-3 pt-2 pb-1"
         >
-          Andy Warhol
+          Marcel Duchamp
         </h1>
 
         <ArtistProfile
