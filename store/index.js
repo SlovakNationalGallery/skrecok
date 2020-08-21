71c5ac82f9a57b7b35f931903b9c7085e3e66fba
Martin Havala <martin.havala@gmail.com>
2020-08-21 16:53:24 +0200

    fixed image paths

--- store/index.js
+++ store/index.js
@@ -18,49 +18,49 @@ export const state = () => ({
     'Anna Daučíková': {
         title: 'Anna Daučíková',
         subtitle: '✸ 1950, Bratislava',
-        avatarSrc: 'img/avatar-daucikova.png',
+        avatarSrc: '/img/avatar-daucikova.png',
         linkSrc: 'http://artbase.kunsthallebratislava.sk/umelec/1918',
         workList: 'daucikova'
     },
     'Pablo Picasso': {
         title: 'Pablo Picasso',
         subtitle: '✸ 1881, Malaga, Španielsko <br> ✝ 1973, Mougins, Francúzsko',
-        avatarSrc: 'img/avatar-picasso.png',
+        avatarSrc: '/img/avatar-picasso.png',
         linkSrc: 'https://www.webumenia.sk/autor/7821',
         workList: 'picasso'
     },
     'Roman Ondak': {
         title: 'Roman Ondak',
         subtitle: '✸ 1966, Žilina',
-        avatarSrc: 'img/avatar-ondak.png',
+        avatarSrc: '/img/avatar-ondak.png',
         linkSrc: 'http://artbase.kunsthallebratislava.sk/umelec/7446',
         workList: 'ondak'
     },
     'Vladimír Dedeček': {
         title: 'Vladimír Dedeček',
         subtitle: '✸ 1929, Martin',
-        avatarSrc: 'img/avatar-dedecek.png',
+        avatarSrc: '/img/avatar-dedecek.png',
         linkSrc: 'https://www.webumenia.sk/en/autor/1052763',
         workList: 'dedecek'
     },
     'Marcel Duchamp': {
         title: 'Marcel Duchamp',
         subtitle: '✸ 1887, Blainville-Crevon, Francúzsko <br> ✝ 1968, Neuilly-sur-Seine, Francúzsko',
-        avatarSrc: 'img/avatar-duchamp.png',
+        avatarSrc: '/img/avatar-duchamp.png',
         linkSrc: 'https://sk.wikipedia.org/wiki/Marcel_Duchamp',
         workList: 'duchamp'
     },
     'Andy Warhol': {
         title: 'Andy Warhol',
         subtitle: '✸ 1928, Pittsburgh, USA <br> ✝ 1987, New York, USA',
-        avatarSrc: 'img/avatar-warhol.png',
+        avatarSrc: '/img/avatar-warhol.png',
         linkSrc: 'https://www.webumenia.sk/autor/11636',
         workList: 'warhol'
     },
     'Maria Bartuszová': {
         title: 'Maria Bartuszová',
         subtitle: '✸ 1936, Praha <br> ✝ 1996, Košice',
-        avatarSrc: 'img/avatar-bartuszova.png',
+        avatarSrc: '/img/avatar-bartuszova.png',
         linkSrc: 'https://www.webumenia.sk/autor/513',
         workList: 'bartuszova'
     },
